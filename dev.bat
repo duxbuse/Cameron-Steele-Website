5e6e342947a8f30f0f957aaff5674ee5025303d0
Sean <duxbuse@gmail.com>
2017-03-29 02:57:42 +1100

    Added a music player to the home page

---
+++ dev.bat
@@ -0,0 +1,4 @@
+
+c: && cd "C:\Users\sean\Dropbox\Cameron Steele website\Website\express_example" && start "nodemon" nodemon npm run
+start .
+start http://localhost:3000
---
+++ public/javascripts/myplaylist.js
@@ -0,0 +1,77 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'/Music/portfolio-piece/Main Portfolio Piece.mp3',
+        oga:'',
+        title:'Portfolio Piece',
+        artist:'Cameron Steele',
+        rating:5,
+        buy:'/#',
+        price:'0.99',
+        duration:'4:59',
+        cover:'/images/steele-logo.jpg'
+    },    
+    {
+        mp3:'/Music/Other Songs/Atmospheric Cave (Original).mp3',
+        oga:'',
+        title:'Atmospheric Cave',
+        artist:'Cameron Steele',
+        rating:4,
+        buy:'/#',
+        price:'0.99',
+        duration:'1:20',
+        cover:'/images/steele-logo.jpg'
+    },    
+    {
+        mp3:'/Music/Other Songs/Battle Song(v2).mp3',
+        oga:'',
+        title:'Battle Song',
+        artist:'Cameron Steele',
+        rating:3,
+        buy:'/#',
+        price:'0.99',
+        duration:'2:40',
+        cover:'/images/steele-logo.jpg'
+    },    
+    {
+        mp3:'/Music/Other Songs/Dark Synthetic Cave (Original).mp3',
+        oga:'',
+        title:'Dark Synthetic Cave',
+        artist:'Cameron Steele',
+        rating:3,
+        buy:'/#',
+        price:'0.99',
+        duration:'2:24',
+        cover:'/images/steele-logo.jpg'
+    },
+    {
+        mp3:'/Music/Other Songs/Don\'t Get Seen (Original).mp3',
+        oga:'',
+        title:'Don\'t Get Seen',
+        artist:'Cameron Steele',
+        rating:4,
+        buy:'/#',
+        price:'0.99',
+        duration:'1:08',
+        cover:'/images/steele-logo.jpg'
+    },
+    {
+        mp3:'/Music/Other Songs/Happy Ice Town (Original).mp3',
+        oga:'',
+        title:'Happy Ice Town',
+        artist:'Cameron Steele',
+        rating:3,
+        buy:'/#',
+        price:'0.99',
+        duration:'1:39',
+        cover:'/images/steele-logo.jpg'
+    }
+];
