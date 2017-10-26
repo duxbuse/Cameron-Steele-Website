efbc75cd6ec2882d2a3b5ab99413133e33f4c4ea
Sean <duxbuse@gmail.com>
2017-10-27 10:14:24 +1100

    moved the test email sending to the server side

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,18 @@ router.get('/contact', function(req, res){
   });
 });
 
+router.post('/contact', function(req, res){
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: 'duxbuse@gmail.com',
+      from: 'admin@steele-music.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    
+    sgMail.send(msg);
+});
+
 module.exports = router;
