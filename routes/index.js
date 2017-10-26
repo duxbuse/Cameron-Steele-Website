9dd49db333e651f69f0402cb71edd54c8a617582
Sean <duxbuse@gmail.com>
2017-10-27 10:40:29 +1100

    reverting back will have to spend more time later

--- routes/index.js
+++ routes/index.js
@@ -25,28 +25,28 @@ router.get('/contact', function(req, res){
   });
 });
 
-router.post('/contact', function(req, res){
-    console.log(req.body.message.name)
-    console.log(req.body.message.emailaddress)
-    console.log(req.body.message.subject)
-    console.log(req.body.message.content)
+// router.post('/contact', function(req, res){
+//     console.log(req.body.message.name)
+//     console.log(req.body.message.emailaddress)
+//     console.log(req.body.message.subject)
+//     console.log(req.body.message.content)
 
-    // const sgMail = require('@sendgrid/mail');
-    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    // const msg = {
-    //   to: 'duxbuse@gmail.com',
-    //   from: 'admin@steele-music.com',
-    //   subject: 'Sending with SendGrid is Fun',
-    //   text: 'and easy to do anywhere, even with Node.js',
-    //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    // };
+//     // const sgMail = require('@sendgrid/mail');
+//     // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//     // const msg = {
+//     //   to: 'duxbuse@gmail.com',
+//     //   from: 'admin@steele-music.com',
+//     //   subject: 'Sending with SendGrid is Fun',
+//     //   text: 'and easy to do anywhere, even with Node.js',
+//     //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+//     // };
     
-    // sgMail.send(msg);
-    // console.log("Form Message has been sent")
+//     // sgMail.send(msg);
+//     // console.log("Form Message has been sent")
 
-    res.render('sentMessage', {
-      title: 'sentMessage'
-    });
-});
+//     res.render('sentMessage', {
+//       title: 'sentMessage'
+//     });
+// });
 
 module.exports = router;
